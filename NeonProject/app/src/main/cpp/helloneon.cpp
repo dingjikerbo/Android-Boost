37026fb3be01f5fc594788572047e9837bd6dbaa
liwentian <liwentian.ai@bytedance.com>
2018-03-20 18:47:37 +0800

    gradle config

--- NeonProject/app/src/main/cpp/helloneon.cpp
+++ NeonProject/app/src/main/cpp/helloneon.cpp
@@ -1,13 +1,21 @@
 #include <jni.h>
 #include <string>
 
-extern "C"
-JNIEXPORT jstring
+#include <arm_neon.h>
 
-JNICALL
+int array1[20] = {
+        2, 1, 4, 16
+};
+
+extern "C"
+JNIEXPORT jstring JNICALL
 Java_com_example_inuker_neon_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    int32x4_t a = vld1q_s32(array1);
+    int16x4_t c = vqshrn_n_s32(a, 1);
+
     return env->NewStringUTF(hello.c_str());
 }
